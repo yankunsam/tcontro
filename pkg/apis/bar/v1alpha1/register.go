09351b397c26156f5c590c0d560c05530cf7317b
yankun <yankun@ulaidao.com>
2019-09-25 18:16:02 +0800

    [typo]: typo fix
    missing ","
    
    Signed-off-by: yankun@ulaidao.com
    Acked-by: yankun@ulaidao.com

--- pkg/apis/bar/v1alpha1/register.go
+++ pkg/apis/bar/v1alpha1/register.go
@@ -1,50 +1,48 @@
 package v1alpha1
 
 import (
-  metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-  "k8s.io/apimachinery/pkg/runtime"
-  "k8s.io/apimachinery/pkg/runtime/schema"
-
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-
 var SchemeGroupVersion = schema.GroupVersion{
-  Group: "bar",
-  Version: "v1alpha1"
+	Group:   "bar",
+	Version: "v1alpha1",
 }
 
 var (
-  SchemeBuilder runtime.SchemeBuilder
-  localSchemeBuilder = &SchemeBuilder
-  AddToScheme = localSchemeBuilder.AddToScheme
+	SchemeBuilder      runtime.SchemeBuilder
+	localSchemeBuilder = &SchemeBuilder
+	AddToScheme        = localSchemeBuilder.AddToScheme
 )
 
-func init()  {
-  localSchemeBuilder.Regisrer(addKnownTypes)
+func init() {
+	localSchemeBuilder.Regisrer(addKnownTypes)
 
 }
 
-func Resource(resource string) schema.GroupResource  {
-  return SchemeGroupVersion.WithResource(resource).GroupResource()
+func Resource(resource string) schema.GroupResource {
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
 
 }
 
-func addKnownTypes(scheme *runtime.Scheme) error  {
-  scheme.AddKnownTypes(
-    SchemeGroupVersion,
-    &HelloType{},
-    &HelloTypeList{}
-  )
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(
+		SchemeGroupVersion,
+		&HelloType{},
+		&HelloTypeList{},
+	)
 
-  scheme.AddKnownTypes(
-    SchemeGroupVersion,
-    &metav1.Status{}
-  )
+	scheme.AddKnownTypes(
+		SchemeGroupVersion,
+		&metav1.Status{},
+	)
 
-  metav1.AddToGroupVersion(
-    scheme,
-    SchemeGroupVersion
-  )
-  return nil
+	metav1.AddToGroupVersion(
+		scheme,
+		SchemeGroupVersion,
+	)
+	return nil
 
 }
